b75ed49b8b8db375a812bd3ad23081383b50bda7
John <43251090+uhwang@users.noreply.github.com>
2024-05-11 13:58:54 -0400

    Auto capture

--- capture.py
+++ capture.py
@@ -10,14 +10,15 @@
 '''
 import sys
 import os
-from PyQt5.QtCore import Qt, pyqtSignal, QObject, QSize              
+import time
+from PyQt5.QtCore import Qt, pyqtSignal, QObject, QSize, QTimer              
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import ( 
         QApplication, QWidget    , QStyleFactory , 
         QPushButton , QLineEdit  , QPlainTextEdit, 
         QComboBox   , QGridLayout, QVBoxLayout   , 
         QHBoxLayout , QFormLayout, QFileDialog   , 
-        QMessageBox , QLabel      
+        QMessageBox , QLabel     , QCheckBox 
         )
 import keyboard
 import pygetwindow
@@ -33,16 +34,22 @@ hot_key_list = ['f2' , 'f3' , 'f3', 'f4',
 file_template = "%s-%03d.png"              
 
 class CaptureCallback(QObject):
-    print_message =  pyqtSignal(str)
+    print_message  = pyqtSignal(str)
     number_changed = pyqtSignal(int)
     
-    def __init__(self, title, hot_key, img_num, prefix):
+    def __init__(self, title, hot_key, img_num, prefix, interval=0):
         super(CaptureCallback, self).__init__()
         self.title = title
         self.hot_key = hot_key
         self.image_number = img_num
         self.prefix  = prefix
-    
+        self.interval = interval
+        self.timer = None
+        
+        if self.interval > 0:
+            self.timer = QTimer(self)
+            self.timer.timeout.connect(self.save)
+           
     def save(self):
         try:
             win = pygetwindow.getWindowsWithTitle(self.title)[0]
@@ -63,7 +70,10 @@ class CaptureCallback(QObject):
             
             im = im.crop((left, top, right, bottom))
             im.save(str(path))
-   
+
+            if isinstance(self.timer, QTimer):
+                keyboard.send(self.hot_key)
+            
         except Exception as e:
             self.print_message.emit(str(e))
             return
@@ -81,16 +91,24 @@ class CaptureCallback(QObject):
     def start(self):
         # on_press returns a hook that can be used to "disconnect" the callback
         # function later, if required
-        self.hook = keyboard.on_press(self.keyboardEventReceived)
+        if isinstance(self.timer, QTimer):
+            self.timer.start(self.interval)
+        else:
+            self.hook = keyboard.on_press(self.keyboardEventReceived)
         
     def stop(self):
-        keyboard.unhook(self.hook)
+        if isinstance(self.timer, QTimer):
+            self.timer.stop()
+            self.timer = None
+        else:
+            keyboard.unhook(self.hook)
         
 class ScreenCapture(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.image_number = 0
+        self.npage_auto_saved = 0
         
     def initUI(self):
         self.form_layout = QFormLayout()
@@ -133,6 +151,20 @@ class ScreenCapture(QWidget):
         self.hot_key.addItems(hot_key_list)
         paper.addWidget(self.hot_key, 4,1)
         
+        paper.addWidget(QLabel("Auto Save"), 5, 0)
+        self.auto_save = QCheckBox()
+        paper.addWidget(self.auto_save, 5,1)
+        self.auto_save.stateChanged.connect(self.audosave_state_changed)        
+        paper.addWidget(QLabel("Interval(ms)"), 6, 0)
+        self.interval = QLineEdit("0.0")
+        self.interval.setEnabled(False)
+        paper.addWidget(self.interval, 6, 1)
+        
+        paper.addWidget(QLabel("Num Pages"), 7, 0)
+        self.npage_to_save = QLineEdit('0')
+        self.npage_to_save.setEnabled(False)
+        paper.addWidget(self.npage_to_save, 7, 1)
+        
         bv = QHBoxLayout()
         
         self.start_capture_btn = QPushButton('Start')
@@ -158,6 +190,14 @@ class ScreenCapture(QWidget):
         self.setWindowIcon(QIcon(QPixmap(icon_capture.table)))
         self.show()
 
+    def audosave_state_changed(self):
+        if self.auto_save.isChecked():
+            self.interval.setEnabled(True)
+            self.npage_to_save.setEnabled(True)
+        else:
+            self.interval.setEnabled(False)
+            self.npage_to_save.setEnabled(False)
+        
     def refresh_applications(self):
         self.application.clear()
         app_list = pygetwindow.getAllTitles()
@@ -176,13 +216,17 @@ class ScreenCapture(QWidget):
         os.chdir(path)
 
     def start_capture(self):
+        self.npage_auto_saved = 0
         self.image_number = int(self.start_number.text())
+        _interval = int(self.interval.text()) if self.auto_save.isChecked() else 0
         self.callback = CaptureCallback(
                             self.application.currentText(),
                             self.hot_key.currentText(),
                             self.image_number,
-                            self.prefix.text()
+                            self.prefix.text(),
+                            _interval
                         )
+
         self.callback.print_message.connect(self.print_concurrent_message)
         self.callback.number_changed.connect(self.set_image_number)
         self.start_capture_btn.setEnabled(False)
@@ -193,10 +237,15 @@ class ScreenCapture(QWidget):
             self.callback.stop()
             self.callback = None
             self.start_capture_btn.setEnabled(True)
+            self.message.appendPlainText("Done ...")
     
     def set_image_number(self, img_num):
         self.start_number.setText("%d"%img_num)
-        self.image_number = img_num
+        self.npage_auto_saved += 1
+        
+        npage = int(self.npage_to_save.text())
+        if npage > 0 and self.npage_auto_saved == npage:
+            self.stop_capture()
         
     def print_concurrent_message(self, con_msg):
         self.message.appendPlainText(con_msg)
